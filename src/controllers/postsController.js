7b38a2cb696a19f60042078575541eba1218b57f
Renato Martins <renatofm94@gmail.com>
2024-11-25 14:16:06 -0300

    add muler and create upload

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
 import { getTodosOsPosts, criarPost } from "../models/postsModel.js";
+import fs from "fs";
 
 export async function listarPosts(req, res) {
     // Chama a função para obter todos os posts e armazena os resultados.
@@ -18,4 +19,25 @@ export async function postarNovoPost(req, res) {
         
     }
 }
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao:"",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
+    try{
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Error":"Falha na requisição"})
+        
+    }
+}
+
 
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,13 +1,41 @@
-import express from "express";
-import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
+import express from "express"; // Importa o framework Express para criar a aplicação web
+import multer from "multer"; // Importa o Multer para lidar com o upload de arquivos
 
+// Importa as funções para lidar com posts do controlador
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+// Configura o armazenamento dos arquivos enviados
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Define o diretório onde os arquivos serão salvos
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    // Define o nome do arquivo salvo (mantém o nome original)
+    cb(null, file.originalname);
+  }
+})
+
+// Cria uma instância do Multer com a configuração de armazenamento
+const upload = multer({ dest: "./uploads", storage });
+
+// **Observação:** A linha comentada abaixo poderia ser utilizada para sistemas Linux/Mac
+// const upload = multer({dest: "./uploads"})
+
+// Define as rotas da aplicação
 const routes = (app) => {
-    // Habilita o middleware para analisar o corpo das requisições que contêm dados no formato JSON.
-    app.use(express.json());
-    // Define uma rota GET para a URL "/posts" buscando todos os posts.
-    app.get("/posts", listarPosts);
-    // Define umma rota POST para criar um post 
-    app.post("/posts", postarNovoPost)
+  // Habilita o parsing de dados JSON nas requisições
+  app.use(express.json());
+
+  // Rota GET para listar todos os posts
+  app.get("/posts", listarPosts);
+
+  // Rota POST para criar um novo post
+  app.post("/posts", postarNovoPost);
+
+  // Rota POST para fazer upload de uma imagem
+  // O middleware `upload.single('imagem')` extrai o arquivo enviado com o nome 'imagem'
+  app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
-export default routes;
+export default routes; // Exporta a função das rotas para ser utilizada em outro arquivo
